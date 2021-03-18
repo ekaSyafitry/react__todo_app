d0844c3653c031d430ecdec7c2706b34dc112660
ekasyafitry <eka.dewi@mncgroup.com>
2021-03-18 16:49:34 +0700

    dfdsf

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 let url = process.env.REACT_APP_API_URL
+
+console.log(process.env.NODE_ENV, 'envvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv')
 if(process.env.NODE_ENV !== 'development') {
     url = 'https://todos-app-mnc.herokuapp.com/'
 }
-
+console.log(url, 'urllllllllllllllllllllllllllllllllllllllllllllll')
 const instance = axios.create({
     baseURL: url
 })
